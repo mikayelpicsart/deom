fb04f180b53141dcf3dbcaa3cc4240be20e4c311
Mikayel Abrahamyan <ma@MikayelAbrahamyanQ2.local>
2020-06-06 14:35:16 +0400

    fix

--- cplus/pre.js
+++ cplus/pre.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 Module['locateFile'] = function (path, scriptDirectory) {
+    typeof readyPromiseReject === 'function'; // for eslint use readyPromiseReject 
     if(['wasm', 'map'].includes(path.split('.').pop())) {
         return path;
     }
